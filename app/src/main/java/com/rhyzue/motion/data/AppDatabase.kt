700d7f4d6073a966989ce2e0b699cd7f7be2b015
Rhyzue <Rennie6710@gmail.com>
2020-04-16 13:03:30 -0400

    WIP: database

---
+++ app/src/main/java/com/rhyzue/motion/data/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.rhyzue.motion.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun TaskDao(): TaskDao
+
+    companion object {
+        //writes to this field are immediately made visible to other threads
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/rhyzue/motion/data/Task.kt
@@ -0,0 +1,50 @@
+package com.rhyzue.motion.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import java.util.*
+
+@Entity
+data class Task(
+    @PrimaryKey val id: Int,
+    @ColumnInfo val name: String,
+    @ColumnInfo val type: Int?,
+    @ColumnInfo val date_assigned: Date,
+    @ColumnInfo val complete: Boolean,
+    @ColumnInfo val deadline: Date?,
+    @ColumnInfo val auto_push: Boolean,
+    @ColumnInfo val goal_id: Int?
+)
+
+@Dao
+interface TaskDao{
+    @Query("SELECT * FROM task")
+    fun getAll(): LiveData<List<Task>>
+
+    @Query("SELECT * FROM task WHERE date_assigned = (:date)")
+    fun getTaskByDate(date: Date): LiveData<List<Task>>
+
+    @Query("SELECT * FROM task WHERE complete = (:complete)")
+    fun getTaskByCompletion(complete: Boolean): LiveData<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(task: Task)
+
+}
+
+class TaskRepository(private val taskDao: TaskDao) {
+
+    val allTasks: LiveData<List<Task>> = taskDao.getAll()
+
+    fun getTaskByDate(date: Date) {
+        taskDao.getTaskByDate(date)
+    }
+
+    fun getTaskByCompletion(complete: Boolean) {
+        taskDao.getTaskByCompletion(complete)
+    }
+
+    suspend fun insert(task: Task){
+        taskDao.insert(task)
+    }
+}
