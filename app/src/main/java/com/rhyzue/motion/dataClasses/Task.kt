738b28ecfca134ab170e27a6d46b0b38261f076a
Rhyzue <Rennie6710@gmail.com>
2020-04-15 22:18:41 -0400

    Task class

--- app/src/main/java/com/rhyzue/motion/dataClasses/Task.kt
+++ app/src/main/java/com/rhyzue/motion/dataClasses/Task.kt
@@ -1,13 +1,28 @@
 package com.rhyzue.motion.dataClasses
 
+import androidx.room.*
 import java.util.*
 
+@Entity
 data class Task(
-    val id: Int,
-    val name: String,
-    val type: Int?,
-    val date_assigned: Date,
-    val deadline: Date?,
-    val auto_push: Boolean,
-    val goal_id: Int?
+    @PrimaryKey val id: Int,
+    @ColumnInfo val name: String,
+    @ColumnInfo val type: Int?,
+    @ColumnInfo val date_assigned: Date,
+    @ColumnInfo val complete: Boolean,
+    @ColumnInfo val deadline: Date?,
+    @ColumnInfo val auto_push: Boolean,
+    @ColumnInfo val goal_id: Int?
 )
+
+@Dao
+interface TaskDao{
+    @Query("SELECT * FROM task")
+    fun getAll(): List<Task>
+
+    @Query("SELECT * FROM task WHERE date_assigned = (:date)")
+    fun getTaskByDate(date: Date): List<Task>
+
+    @Query("SELECT * FROM task WHERE complete = (:complete)")
+    fun getTaskByCompletion(complete: Boolean): List<Task>
+}
