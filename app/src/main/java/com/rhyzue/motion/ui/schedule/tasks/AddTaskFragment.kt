f439849a485e5ab0818ada90f169f8e9d68e5464
Rhyzue <Rennie6710@gmail.com>
2020-04-28 20:02:57 -0400

    editText and dropdown

--- app/src/main/java/com/rhyzue/motion/ui/schedule/tasks/AddTaskFragment.kt
+++ app/src/main/java/com/rhyzue/motion/ui/schedule/tasks/AddTaskFragment.kt
@@ -2,30 +2,63 @@ package com.rhyzue.motion.ui.schedule.tasks
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
 import com.rhyzue.motion.R
 
-class AddTaskFragment : DialogFragment() {
+class AddTaskFragment : DialogFragment(), AdapterView.OnItemSelectedListener {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
         return activity?.let {
-            // Use the Builder class for convenient dialog construction
             val builder = AlertDialog.Builder(it)
             val inflater = requireActivity().layoutInflater;
             builder.setView(inflater.inflate(R.layout.add_task_fragment, null))
                 .setPositiveButton("Ok",
                     DialogInterface.OnClickListener { dialog, id ->
-                        // FIRzE ZE MISSILES!
+                        getDialog()?.cancel()
                     })
                 .setNegativeButton("cancel",
                     DialogInterface.OnClickListener { dialog, id ->
-                        // User cancelled the dialog
+                        getDialog()?.cancel()
                     })
-            // Create the AlertDialog object and return it
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        val dialog = requireDialog()
+
+        //create spinner
+        val spinner: Spinner = dialog.findViewById(R.id.types_spinner)
+        context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.types_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+            }
+        }
+        spinner ?. let {spinner.onItemSelectedListener = this}
+
+
+    }
+
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        println("selected type")
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        println("didn't select type")
     }
 }
