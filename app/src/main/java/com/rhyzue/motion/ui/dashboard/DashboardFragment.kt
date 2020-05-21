8d9efe347b4a1fcc9ada96fc63e1b38eb9c84041
Rhyzue <Rennie6710@gmail.com>
2020-05-21 13:56:47 -0400

    add graph

--- app/src/main/java/com/rhyzue/motion/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/rhyzue/motion/ui/dashboard/DashboardFragment.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 import com.pnikosis.materialishprogress.ProgressWheel
 import com.rhyzue.motion.R
 import com.rhyzue.motion.data.Task
@@ -21,6 +23,8 @@ class DashboardFragment : Fragment() {
     private lateinit var dateTextView: TextView
     private lateinit var progressTextView: TextView
     private lateinit var progressBar: ProgressWheel
+    private lateinit var graph: GraphView
+    private lateinit var series: LineGraphSeries<DataPoint>
     private val df: SimpleDateFormat = SimpleDateFormat("MMM dd yyyy")
 
     override fun onCreateView(
@@ -37,6 +41,19 @@ class DashboardFragment : Fragment() {
 
         dashboardViewModel.setTasks(Calendar.getInstance().time)
         dateTextView.text = df.format(Calendar.getInstance().time)
+
+        graph = dashboard.findViewById(R.id.graph)
+        series =
+            LineGraphSeries(
+                arrayOf(
+                    DataPoint(0.0, 1.0),
+                    DataPoint(1.0, 5.0),
+                    DataPoint(2.0, 3.0)
+                )
+            )
+        graph.addSeries(series)
+        graph.gridLabelRenderer.verticalAxisTitle = "Number of tasks"
+        graph.gridLabelRenderer.horizontalAxisTitle = "Date"
 
         activity?.let{
             dashboardViewModel.todayTasks.observe(it, Observer{ tasks: List<Task> ->
